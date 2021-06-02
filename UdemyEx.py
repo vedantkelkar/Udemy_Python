b0571d8c6451497933067a4f425bd421ffd105c7
vedantkelkar <71768118+vedantkelkar@users.noreply.github.com>
2021-06-02 11:48:12 +0530

    Add files via upload

---
+++ UdemyEx.py
@@ -0,0 +1,6 @@
+#Ex1 Strings
+firstn=input("What is your first name")
+midn=input("What is your middle name")
+lastn=input("What is your last name")
+
+print("Initials are ",firstn[0],midn[0],lastn[0])            
